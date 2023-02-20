49fa8bf613d221196d3d32f85c3b68cf1ae6ba65
TychoBrouwer <tychobrouwer33@gmail.com>
2023-02-20 23:49:08 +0100

    test

--- test-client/get-device.py
+++ test-client/get-device.py
@@ -5,7 +5,7 @@ from constants import *
 
 # Set url and data for request (for getting device settings)
 # url = f'http://{SERVER_IP}:{SERVER_FLASK_PORT}/get-device'
-url = f'http://{SERVER_ADDRESS}/get-device'
+url = f'https://{SERVER_ADDRESS}/get-device'
 Data = {
   'ClientID': 'CLIENT IDENTIFIER',
   'DeviceID': 'DEVICE IDENTIFIER',
