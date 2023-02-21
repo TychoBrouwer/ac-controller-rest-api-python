5f8d20e01ae517ea627905cb955b3536dda5d739
TychoBrouwer <tychobrouwer33@gmail.com>
2023-02-21 16:14:48 +0100

    fix initiation function undefined

--- socket_connection.py
+++ socket_connection.py
@@ -27,10 +27,10 @@ class SocketConnection:
 
     async def socket_handler(self, websocket):
         # Send server connection conformation to device
-        await self.send(websocket, str.encode('Server is working!'))
+        await websocket.send(str.encode('Server is working!'))
 
         # Receive device identifier from device
-        deviceID = await self.receive(websocket)
+        deviceID = await websocket.recv()
         if deviceID:
             # Store device identifier in currently connected dict
             self.devices[deviceID] = websocket
