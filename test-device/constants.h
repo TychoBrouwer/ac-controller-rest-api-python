5dbd9869d8df04a375b75c9f1f6e481a456fb48d
TychoBrouwer <tychobrouwer33@gmail.com>
2023-02-22 14:17:11 +0100

    device test file transflation to cpp

---
+++ test-device/constants.h
@@ -0,0 +1,2 @@
+char SERVER_SOCKET_ADDRESS[] = "accontroller-socket.tbrouwer.com";
+char SERVER_API_ADDRESS[] = "accontroller-api.tbrouwer.com";
---
+++ test-device/test-device.ino
@@ -0,0 +1,80 @@
+// Library used for wifi connection
+#include <WiFi.h>
+// Library used for web socket connection (https://github.com/morrissinger/ESP8266-Websocket)
+#include "WebSocketClient.h"
+
+// Include constants
+#include "./constants.h"
+
+// WiFi credentials
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+
+// Web socket client
+WebSocketClient webSocketClient;
+// WiFi client
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  delay(5000);
+ 
+  if (client.connect(SERVER_SOCKET_ADDRESS, 443)) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Connection failed.");
+  }
+ 
+  // perform handshake with server
+  // webSocketClient.path = '';
+  webSocketClient.host = SERVER_SOCKET_ADDRESS;
+  if (webSocketClient.handshake(client)) {
+    Serial.println("Handshake successful");
+  } else {
+    Serial.println("Handshake failed.");
+  }
+
+  if (client.connected()) {
+    // Receive server conformation
+    String data;
+    webSocketClient.getData(data);
+    Serial.println(data);
+
+    // Send device identifier to server
+    webSocketClient.sendData("DEVICE IDENTIFIER");
+  }
+}
+ 
+void loop() {
+  String data;
+ 
+  if (client.connected()) {
+    // Receive data from server
+    webSocketClient.getData(data);
+    Serial.println(data);
+
+    // If get settings request is received send settings to server  
+    if (data == "get-settings") {
+        webSocketClient.sendData("DEVICE IDENTIFIER");
+    }
+  } else {
+    Serial.println("Client disconnected.");
+  }
+ 
+  delay(500);
+}
