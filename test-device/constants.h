3aadf6952e914cc1af69bac7190a0c43ac474efe
TychoBrouwer <tychobrouwer33@gmail.com>
2023-02-23 16:40:29 +0100

    test with callback function

--- test-device/constants.h
+++
@@ -1,5 +0,0 @@
-// WiFi credentials
-char* WIFI_SSID = "SSID";
-char* WIFI_PASSWORD = "PASSWORD";
-
-const char SERVER_ADDRESS[] = "accontroller.tbrouwer.com";
--- test-device/test-device.ino
+++ test-device/test-device.ino
@@ -1,12 +1,28 @@
-// Library used for wifi connection
-#include <WiFi.h>
-// Library used for web socket connection (https://github.com/gilmaimon/ArduinoWebsockets)
-#include <ArduinoWebsockets.h>
-// Library used for JSON parsing
-#include <ArduinoJson.h>
+#include <WiFi.h> // Library used for wifi connection
+#include <ArduinoWebsockets.h> // Library used for web socket connection (https://github.com/gilmaimon/ArduinoWebsockets)
+#include <ArduinoJson.h> // Library used for JSON parsing 
 
-// Include constants
-#include "./constants.h"
+#include <Arduino.h> 
+#include <IRremoteESP8266.h> // Library used for IR transmission
+#include <IRsend.h> // Library used for IR transmission
+
+// WiFi credentials
+char* WIFI_SSID = "SSID";
+char* WIFI_PASSWORD = "PASSWORD";
+// Websocked server address
+const char* SERVER_ADDRESS = "wss://accontroller.tbrouwer.com:443/ws";
+
+// IR LED pin
+const uint16_t kIrLed = 2;
+IRsend irsend(kIrLed);
+
+// LED pins
+const int Red_LED_pin=32;
+const int Green_LED_pin=33;
+
+// Example IR codes for Mitsubishi AC
+uint8_t mitsubishiState_on[19]={0xAD, 0x51, 0x3C, 0xE5, 0x1A, 0x0C, 0xF3, 0x05, 0xFA, 0x00, 0xFF, 0xC0, 0x3F, 0xC8, 0x37, 0x00, 0xFF, 0x80, 0x7F};
+uint8_t mitsubishiState_off[19]={0xAD, 0x51, 0x3C, 0xE5, 0x1A, 0x04, 0xFB, 0x05, 0xFA, 0x00, 0xFF, 0xC0, 0x3F, 0xC8, 0x37, 0x00, 0xFF, 0x80, 0x7F};
 
 // Web socket client
 using namespace websockets;
@@ -15,11 +31,70 @@ WebsocketsClient client;
 // Settings JSON object
 DynamicJsonDocument settingsJson(1024);
 
-// WiFi credentials
-char* WIFI_SSID = "SSID";
-char* WIFI_PASSWORD = "PASSWORD";
+void onMessageCallback(WebsocketsMessage request) {
+  Serial.println(request.data());
 
-const char* SERVER_ADDRESS = "wss://accontroller.tbrouwer.com:443/ws";
+  // Check if server is working
+  if (request.data() == "Server is working!") {
+    // Send device ID to server
+    client.send(settingsJson["deviceID"].as<const char*>());
+
+    // Return early
+    return;
+  }
+
+  // Parse request data
+  DynamicJsonDocument requestJson(1024);
+  deserializeJson(requestJson, request.data());
+  
+  if (requestJson['op'] == "get-settings") {
+    // Serialize current settings to string
+    String settingsString;
+    serializeJson(settingsJson, settingsString);
+
+    // Send settings to server
+    client.send(settingsString);
+  } else if (requestJson['op'] == "update-settings") {
+      // Update settings
+      JsonVariant settingsToUpdate = requestJson['settings'];
+
+      // Iterate through settings to update
+      for (JsonPair pair : settingsToUpdate.as<JsonObject>()) {
+        settingsJson[pair.key()] = pair.value();
+      }
+  }
+
+  return;
+}
+
+void onEventsCallback(WebsocketsEvent event, String data) {
+    if(event == WebsocketsEvent::ConnectionOpened) {
+        Serial.println("Connnection Opened");
+    } else if(event == WebsocketsEvent::ConnectionClosed) {
+        Serial.println(client.getCloseReason());
+        Serial.println("Connnection Closed");
+    } else if(event == WebsocketsEvent::GotPing) {
+        Serial.println("Got a Ping!");
+    } else if(event == WebsocketsEvent::GotPong) {
+        Serial.println("Got a Pong!");
+    }
+}
+
+void turn_ac_on(){
+  irsend.sendMitsubishiHeavy152(mitsubishiState_on);
+  delay(500);
+  digitalWrite(Green_LED_pin, HIGH);
+  delay(1500);
+  digitalWrite(Green_LED_pin, LOW);
+}
+
+void turn_ac_off(){
+  irsend.sendMitsubishiHeavy152(mitsubishiState_off);
+  delay(500);
+  digitalWrite(Red_LED_pin, HIGH);
+  delay(1500);
+  digitalWrite(Red_LED_pin, LOW);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -45,42 +120,51 @@ void setup() {
  
   delay(1000);
 
+  // run callback when messages are received
+  client.onMessage(onMessageCallback);
+  
+  // run callback when events are occurring
+  client.onEvent(onEventsCallback);
+
   // Connect to server
   client.connect(SERVER_ADDRESS);
 
-  // Receive server conformation
-  auto conf = client.readBlocking();
+  // // Receive server conformation
+  // auto conf = client.readBlocking();
 
-  // Send a message
-  client.send(settingsJson["deviceID"].as<const char*>());
+  // // Send a message
+  // client.send(settingsJson["deviceID"].as<const char*>());
 }
 
 void loop() {
-  // Receive data from server
-  auto request = client.readBlocking();
-  Serial.println(request.data());
-  Serial.println(request.c_str());
+  // Check for new messages
+  client.poll();
 
-  // Parse request data
-  DynamicJsonDocument requestJson(1024);
-  deserializeJson(requestJson, request.data());
+  // // Receive data from server
+  // auto request = client.readBlocking();
+  // Serial.println(request.data());
+  // Serial.println(request.c_str());
+
+  // // Parse request data
+  // DynamicJsonDocument requestJson(1024);
+  // deserializeJson(requestJson, request.data());
   
-  if (requestJson['op'] == "get-settings") {
-    // Serialize current settings to string
-    String settingsString;
-    serializeJson(settingsJson, settingsString);
+  // if (requestJson['op'] == "get-settings") {
+  //   // Serialize current settings to string
+  //   String settingsString;
+  //   serializeJson(settingsJson, settingsString);
 
-    // Send settings to server
-    client.send(settingsString);
-  } else if (requestJson['op'] == "update-settings") {
-      // Update settings
-      JsonVariant settingsToUpdate = requestJson['settings'];
+  //   // Send settings to server
+  //   client.send(settingsString);
+  // } else if (requestJson['op'] == "update-settings") {
+  //     // Update settings
+  //     JsonVariant settingsToUpdate = requestJson['settings'];
 
-      // Iterate through settings to update
-      for (JsonPair pair : settingsToUpdate.as<JsonObject>()) {
-        settingsJson[pair.key().c_str()] = pair.value();
-      }
-  }
+  //     // Iterate through settings to update
+  //     for (JsonPair pair : settingsToUpdate.as<JsonObject>()) {
+  //       settingsJson[pair.key().c_str()] = pair.value();
+  //     }
+  // }
 
-  delay(100);
+  // delay(100);
 }
