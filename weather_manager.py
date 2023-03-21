d0d1d2c217c47094fcf91fa07a9e4734197ba1c3
HaentjeGH <82949126+HaentjeGH@users.noreply.github.com>
2023-03-21 13:28:26 +0100

    change to request from visualcrossing

--- weather_manager.py
+++ weather_manager.py
@@ -1,17 +1,19 @@
 import requests
-
+import time
 from constants import *
-from private import OPENWEATHERMAP_API_KEY
+from private import VISUALCROSSINGP_API_KEY
 
 
 class WeatherManager:
     def getWeather(self):
-        # Get weather data from openweathermap
+        time_unix = int(time.time())
+        # Get weather data from VisualCrossing
         response = requests.get(
-            f'https://api.openweathermap.org/data/2.5/forecast?q={LOCATION}&cnt={OBSERVATION_COUNT}&appid={OPENWEATHERMAP_API_KEY}&units=metric')
+            f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{LOCATION}/{time_unix}/{time_unix+OBSERVATION_COUNT*3600}?unitGroup=metric&include=hours&key={VISUALCROSSING_API_KEY}&contentType=json'
+        )
         # Check if request was successful
         if response.status_code != 200:
-            print('Error while getting weather data')
+            print('Error while getting weather data, response code: ', response.status_code)
             return None
         # Return weather data
-        return response.json()
+        return (response.json(), time_unix)
